ac42b33b4745a677ffb614cb45cdd319195c195f
Liam Zhou <zhouchengzhong77@gmail.com>
2020-05-11 14:16:52 -0700

    Add error handler for 404

--- flaskr/errors/handlers.py
+++ flaskr/errors/handlers.py
@@ -9,6 +9,11 @@ def unauthorized(error):
     return render_template('errors/401.html'), 401
 
 
+@bp.app_errorhandler(404)
+def not_found(error):
+    return render_template('errors/404.html'), 404
+
+
 @bp.app_errorhandler(500)
 def internal_server_error(error):
     db.session.rollback()
