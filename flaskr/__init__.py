1a2413155cfb412873516def83edd44ad43ad88c
Liam Zhou <zhouchengzhong77@gmail.com>
2020-05-11 16:08:01 -0700

    Change dev database to postgres

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -16,7 +16,10 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev', DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI=(
+            'sqlite:///' + os.path.join(app.instance_path, 'flaskr.db')
+        ),
     )
 
     if test_config is None:
