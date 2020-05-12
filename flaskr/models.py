d180939ae55707217e43984f4a950ebaef65470c
Liam Zhou <zhouchengzhong77@gmail.com>
2020-05-12 13:02:16 -0700

    Add projects table

--- flaskr/models.py
+++ flaskr/models.py
@@ -6,6 +6,14 @@ db = SQLAlchemy()
 login = LoginManager()
 login.login_view = 'auth.login'
 
+# association table for users and projects
+user_project = db.Table(
+    'user_project',
+    db.Column('user_id', db.String(), db.ForeignKey('users.id'), primary_key=True),
+    db.Column('project_id', db.Integer, db.ForeignKey('projects.id'), primary_key=True),
+    db.Column('role', db.String(9), nullable=False),
+)
+
 
 class User(UserMixin, db.Model):
     """Represents users table.
@@ -19,6 +27,12 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(), unique=True, nullable=False)
     first_name = db.Column(db.String(), nullable=False)
     last_name = db.Column(db.String(), nullable=False)
+    projects = db.relationship(
+        'Project',
+        secondary=user_project,
+        lazy='subquery',
+        backref=db.backref("users", lazy=True),
+    )
 
     def __repr__(self):
         return f"<User '{self.first_name} {self.last_name}' '{self.email}'"
@@ -26,6 +40,33 @@ class User(UserMixin, db.Model):
     def insert(self):
         """Inserts user into users table."""
         db.session.add(self)
+        db.session.commit()
+
+
+class Project(db.Model):
+    """Represents projects table."""
+
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Project '{self.id} {self.title}'"
+
+    def insert(self):
+        """Inserts project into projects table."""
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        """Deletes project from projects table."""
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        """Updates project info."""
         db.session.commit()
 
 
