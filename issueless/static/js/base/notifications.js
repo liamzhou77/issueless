be143c1f93735cadc2937fade467411a31ca23e8
Liam Zhou <zhouchengzhong77@gmail.com>
2020-07-23 15:15:04 -0600

    Mark notification as read after redirect

--- issueless/static/js/base/notifications.js
+++ issueless/static/js/base/notifications.js
@@ -198,7 +198,14 @@ function generateNotifications() {
             'material-icons mx-2 notification-redirect-icon';
           redirectIcon.textContent = 'navigate_next';
           redirectIcon.addEventListener('click', function () {
-            window.location.href = `/projects/${data.projectId}/issues/${targetId}`;
+            postFetch(`/notifications/read?id=${notificationId}`, (data2) => {
+              if (data2.success) {
+                window.location.href = `/projects/${data.projectId}/issues/${targetId}`;
+              } else {
+                $('.nav-item .dropdown-toggle').dropdown('hide');
+                addAlert(data2.error, 'danger');
+              }
+            });
           });
           media.appendChild(redirectIcon);
         }
